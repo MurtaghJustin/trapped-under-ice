ee04e167535387b04dc304fc7ab49e7ecf2b73d5
Justin Murtagh <8622894+MurtaghJustin@users.noreply.github.com>
2020-02-08 16:13:49 +1100

    Almost time to start having a bloody game, mate! Well, start making it anyways

--- src/game/board.tsx
+++ src/game/board.tsx
@@ -1,79 +1,19 @@
 import * as React from 'react';
-import { RawLevel, Level, Tile, RawTile } from './models/level';
-import { _mapWidth, _mapHeight } from '../_settings';
+import { GetNonsenseLevel } from './utils';
+import { Pos } from './models/position';
 
 export const Board: React.FC<Props> = (props) => {
+    let level = GetNonsenseLevel();
+    let [position, setPosition] = React.useState(props.start);
 
 
     return (
         <canvas>
-
+            
         </canvas>
     );
 }
 
 interface Props {
-
-}
-
-export const ImportLevel = (rawLvl: RawLevel) => {
-    const level: Tile[][] = [];
-    const groundTile = '/sprites/ground.png';
-    const wallTile = '/sprites/wall/png';
-    const monsterSprites = [
-        '/sprites/slime.png'
-    ];
-
-    for (let y = 0; y < rawLvl.layout.length; y++) {
-        let rawLine = rawLvl.layout[y];
-        const line = [];
-        for (let x = 0; x < rawLine.length; x++) {
-            let tile = {
-                contents: []
-            } as Tile;
-
-            const rawTile = rawLine[x];
-
-            if (rawTile === RawTile.Wall)
-                tile.contents.push({ url: wallTile });
-            else {
-                tile.contents.push({ url: groundTile });
-                const random = Math.floor(Math.random() * monsterSprites.length);
-                
-                switch (rawTile) {
-                    case RawTile.Monster:
-                        tile.contents.push({ url: monsterSprites[random] });
-                    // TODO: The rest
-                }
-            }
-        }
-    }
-
-    return {
-        defaultSprite: groundTile,
-        layout: level
-    } as Level;
-}
-
-export const RandomLevel = (width: number, height: number) => {
-    let level = [];
-
-    for (let y = 0; y < height; y++) {
-        let line = [];
-
-        for (let x = 0; x < width; x++) {
-            line.push(Math.floor(Math.random() * 3));
-        }
-
-        level.push(line);
-    }
-
-    return level;
-}
-
-export const GetNonsenseLevel = () => {
-    let content = RandomLevel(_mapWidth * 4, _mapHeight * 4);
-    return ImportLevel({
-        layout: content
-    });
+    start: Pos;
 }
---
+++ src/game/utils.ts
@@ -0,0 +1,64 @@
+import { RawLevel, Level, Tile, RawTile } from './models/level';
+import { _mapWidth, _mapHeight } from '../_settings';
+
+export const ImportLevel = (rawLvl: RawLevel) => {
+    const level: Tile[][] = [];
+    const groundTile = '/sprites/ground.png';
+    const wallTile = '/sprites/wall/png';
+    const monsterSprites = [
+        '/sprites/slime.png'
+    ];
+
+    for (let y = 0; y < rawLvl.layout.length; y++) {
+        let rawLine = rawLvl.layout[y];
+        const line = [];
+        for (let x = 0; x < rawLine.length; x++) {
+            let tile = {
+                contents: []
+            } as Tile;
+
+            const rawTile = rawLine[x];
+
+            if (rawTile === RawTile.Wall)
+                tile.contents.push({ url: wallTile });
+            else {
+                tile.contents.push({ url: groundTile });
+                const random = Math.floor(Math.random() * monsterSprites.length);
+                
+                switch (rawTile) {
+                    case RawTile.Monster:
+                        tile.contents.push({ url: monsterSprites[random] });
+                    // TODO: The rest
+                }
+            }
+        }
+    }
+
+    return {
+        defaultSprite: groundTile,
+        layout: level
+    } as Level;
+}
+
+export const RandomLevel = (width: number, height: number) => {
+    let level = [];
+
+    for (let y = 0; y < height; y++) {
+        let line = [];
+
+        for (let x = 0; x < width; x++) {
+            line.push(Math.floor(Math.random() * 3));
+        }
+
+        level.push(line);
+    }
+
+    return level;
+}
+
+export const GetNonsenseLevel = () => {
+    let content = RandomLevel(_mapWidth, _mapHeight);
+    return ImportLevel({
+        layout: content
+    });
+}
